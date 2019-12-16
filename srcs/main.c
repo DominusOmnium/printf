a6ef4d4810f55e861e2d994dadab7dc08b775a92
Dake Kathleen <morozov2101@gmail.com>
2019-12-16 17:01:10 +0300

    little changes

--- srcs/main.c
+++ srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: dkathlee <dkathlee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/03 16:53:15 by marvin            #+#    #+#             */
-/*   Updated: 2019/12/03 16:53:15 by marvin           ###   ########.fr       */
+/*   Updated: 2019/12/16 17:00:15 by dkathlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,7 @@ int		is_type(char c)
 	return (false);
 }
 
-void	parcing_format(char **f, t_printf *p)
+void	parcing_format(const char **f, t_printf *p)
 {
 	if (**f == 'd' || **f == 'i')
 		p->type = type_int;
@@ -66,18 +66,23 @@ void	parcing_format(char **f, t_printf *p)
 		p->type = type_percent;
 	if (ft_isupper(**f))
 		p->spec = (**f == 'F') ? sp_L : sp_ll;
-	*f++;
+	(*f)++;
 }
 
-void	check_flags_and_specs(char **f, t_printf *p)
+void	check_flags_and_specs(const char **f, t_printf *p)
 {
 	while (!is_type(**f) && **f)
 	{
-		p->flags.hashtag = **f == '#' ? true : false;
-		p->flags.minus = **f == '-' ? true : false;
-		p->flags.plus = **f == '+' ? true : false;
-		p->flags.space = **f == ' ' ? true : false;
-		p->flags.zero = **f == '0' ? true : false;
+		if (**f == '#')
+			p->flags.hashtag = true;
+		else if (**f == '-')
+			p->flags.minus = true;
+		else if (**f == '+')
+			p->flags.plus = true;
+		else if (**f == ' ')
+			p->flags.space = true;
+		else if (**f == '0')
+			p->flags.zero = true;
 		if (**f == 'h' && **(f + 1) == 'h' && p->spec < sp_hh)
 			p->spec = sp_hh;
 		else if (**f == 'h' && *(*f + 1) != 'h' && p->spec < sp_h)
@@ -90,14 +95,22 @@ void	check_flags_and_specs(char **f, t_printf *p)
 			p->spec = sp_L;
 		else if (is_type(**f))
 			parcing_format(f, p);
-		*f++;
+		(*f)++;
 	}
 }
 
 void	parse_percent(const char **format, t_printf *p)
 {
-	*format++;
+	char	*tmp;
+	char	*tmp1;
+
+	(*format)++;
 	check_flags_and_specs(format, p);
+	tmp = ft_strjoin(p->print, "percent");
+	ft_memdel((void**)&(p->print));
+	p->print_num += 7;
+	p->print = tmp;
+	(*format)++;
 }
 
 void	parse_string(const char **format, t_printf *p)
@@ -115,7 +128,7 @@ void	parse_string(const char **format, t_printf *p)
 	ft_memdel((void**)&(p->print));
 	ft_memdel((void**)&tmp1);
 	p->print = tmp;
-	*format += i + 1;
+	*format += i;
 }
 
 int		ft_printf(const char *format, ...)
@@ -139,8 +152,8 @@ int		ft_printf(const char *format, ...)
 	return (0);
 }
 
-
-int		main()
+int		main(void)
 {
-	return (ft_printf("wqerqwrqwerqwe", 1));
+	ft_printf("befor**% +-lld**after**%f**", 1, 5.0);
+	return (0);
 }
